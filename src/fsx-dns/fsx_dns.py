dd43e10ce09fd3b6fbbba2f41db7969d07b462cd
Franco Rezabek <rezabekf@amazon.co.uk>
2020-11-17 16:18:41 +0000

    Template cleanup

--- src/fsx-dns/fsx_dns.py
+++ src/fsx-dns/fsx_dns.py
@@ -6,23 +6,21 @@ import boto3
 from crhelper import CfnResource
 
 logger = logging.getLogger(__name__)
-helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120)
-fsx = boto3.client('fsx')
+helper = CfnResource(
+    json_logging=False, log_level="DEBUG", boto_level="CRITICAL", sleep_on_delete=120
+)
+fsx = boto3.client("fsx")
 
 
 @helper.create
 def create(event, context):
     logger.info("Got Create")
-    properties = event.get('ResourceProperties', {})
-    fsx_id = properties.get('FSxFileSystemId')
+    properties = event.get("ResourceProperties", {})
+    fsx_id = properties.get("FSxFileSystemId")
 
     fsx_dns_name = _fsx_dns_name(fsx_id)
 
-    helper.Data.update(
-        {
-            "FSxDNSName": fsx_dns_name
-        }
-    )
+    helper.Data.update({"FSxDNSName": fsx_dns_name})
     return fsx_dns_name
 
 
@@ -46,4 +44,4 @@ def _fsx_dns_name(fsx_id):
             fsx_id,
         ]
     )
-    return response['FileSystems'][0]['DNSName']
+    return response["FileSystems"][0]["DNSName"]
