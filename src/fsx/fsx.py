e7c112c7f6cdae9d5cb70511c7a1207d708c171e
Franco Rezabek <rezabekf@amazon.co.uk>
2020-12-02 13:47:06 +0000

    Update FSx custom resource

--- src/fsx/fsx.py
+++ src/fsx/fsx.py
@@ -18,10 +18,9 @@ def create(event, context):
     properties = event.get("ResourceProperties", {})
     fsx_id = properties.get("FSxFileSystemId")
 
-    fsx_dns_name = _fsx_dns_name(fsx_id)
+    dns_name, private_ip = _fsx_describe_file_systems(fsx_id)
 
-    helper.Data.update({"FSxDNSName": fsx_dns_name})
-    return fsx_dns_name
+    helper.Data.update({"FSxDNSName": dns_name, "FSxPrivateIP": private_ip})
 
 
 @helper.update
@@ -38,10 +37,14 @@ def handler(event, context):
     helper(event, context)
 
 
-def _fsx_dns_name(fsx_id):
+def _fsx_describe_file_systems(fsx_id):
     response = fsx.describe_file_systems(
         FileSystemIds=[
             fsx_id,
         ]
     )
-    return response["FileSystems"][0]["DNSName"]
+    dns_name = response["FileSystems"][0]["DNSName"]
+    private_ip = response["FileSystems"][0]["WindowsConfiguration"][
+        "PreferredFileServerIp"
+    ]
+    return dns_name, private_ip
